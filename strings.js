42441b38006a87e4f299228639f7d167bd4b7236
Barry Russell <blrussell09@gmail.com>
2018-02-26 20:31:54 -0600

    Added challenge 2

--- strings.js
+++ strings.js
@@ -1,3 +1,4 @@
-// Challenge 1
-var numOfSandwiches = 4*2
-console.log('numOfSanswiches', numOfSandwiches); //Should print the number 8
+// Challenge 2
+var name = "Barry"
+var string = "Hello, " + name + " how are you doing today?";
+console.log('string' + string);
