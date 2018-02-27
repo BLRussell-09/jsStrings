d66af0dcbc384c20cfb267da5831b5efc2f372db
Barry Russell <blrussell09@gmail.com>
2018-02-26 20:40:21 -0600

    Added Challenge 3

--- strings.js
+++ strings.js
@@ -1,4 +1,15 @@
+// Challenge 1
+var numOfSandwiches = 4*2
+console.log('numOfSanswiches', numOfSandwiches); //Should print the number 8
+
 // Challenge 2
 var name = "Barry"
 var string = "Hello, " + name + " how are you doing today?";
 console.log('string' + string);
+
+// Challenge 3
+
+var dna = "GCAT";
+var rna = dna.replace(/T/, "U");
+
+console.log("RNA", rna);
